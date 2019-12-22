44a0843669a1bde3bfb02ed9914b0c2b7d57e8f4
Hannah G <HannahGooding@hs-macbook-air.home>
2019-12-22 22:33:57 +0000

    Initialising variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert( 'Hello, world!' );
+// initialising variables
+
+const images = document.querySelectorAll(".image") // select multiple elements with specfied class and put them in a node list
+const nextArrow = document.querySelector("#arrow-right"); // select element with specified id
+const previousArrow = document.querySelector("#arrow-left");
+const playPause = document.querySelector("#play-button");
+let auto = false; // automatic scroll option
+const intervalTime = 5000; // time between images in milliseconds
+let imageInterval; // keep track of intervals
