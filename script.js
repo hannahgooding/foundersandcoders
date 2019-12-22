a812548e281e25fac1b2b68c9d15e62eea10bfb4
Hannah G <HannahGooding@hs-macbook-air.home>
2019-12-22 22:37:05 +0000

    Next image and Previous Image functions
    - Define functions to navigate to next image and previous image
    - Add event listeners that perform their respective functions when clicked by user

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ const playPause = document.querySelector("#play-button");
 let auto = false; // automatic scroll option
 const intervalTime = 5000; // time between images in milliseconds
 let imageInterval; // keep track of intervals
+
+// defining functions
+
+function nextImage(){
+  const current = document.querySelector(".current"); // select the element with class .current
+  current.classList.remove("current"); // remove current class
+  if(current.nextElementSibling){ // if the next element is a sibling
+    current.nextElementSibling.classList.add("current"); // add current class to next sibling
+  } else { // when the next element is not a sibling, i.e. the end of the list
+    images[0].classList.add("current"); // add current to the first element
+  }
+  setTimeout(function(){ current.classList.remove("current") }); // remove the current class after a delay
+};
+
+function prevImage(){
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if(current.previousElementSibling){ // if the previous element is a sibling
+    current.previousElementSibling.classList.add("current"); // add current class to previous sibling
+  } else { // when the previous element is not a sibling, i.e. the start of the list
+    images[images.length-1].classList.add("current"); // add current to the last element
+  }
+  setTimeout(function(){ current.classList.remove("current") });
+};
+
+nextArrow.addEventListener("click", function(){
+  nextImage();
+  if(auto){
+    clearInterval(imageInterval); // make interval count start again
+    imageInterval = setInterval(nextImage, intervalTime);
+  }
+});
+
+previousArrow.addEventListener("click", function(){
+  prevImage();
+  if(auto){
+    clearInterval(imageInterval);
+    imageInterval = setInterval(nextImage, intervalTime);
+  }
+});
